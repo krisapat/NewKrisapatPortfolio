4b1dc0b6ed9468b252a3e1033603659287f0b0fe
krisapat <pluemkrisapat@gmail.com>
2025-11-23 16:18:02 +0700

    update main.3

--- utils/schema.ts
+++ utils/schema.ts
@@ -17,7 +17,7 @@ export const validateWithZod = <T extends z.ZodTypeAny>(
 export const ProjectSchema = z.object({
   name: z.string().min(2, { message: "Project name is required 2" }).max(30, { message: "Project name should be less than 30 characters" }),
   descriptionCard: z.string().min(2, { message: "descriptionCard is required 2" }).max(500, { message: "descriptionCard should be less than 500 characters" }),
-  descriptionDetail: z.string().min(2, { message: "descriptionDetail is required 2" }).max(500, { message: "descriptionDetail should be less than 500 characters" }),
+  descriptionDetail: z.string().min(2, { message: "descriptionDetail is required 2" }),
   demoLink: z.string(),
 })
 
